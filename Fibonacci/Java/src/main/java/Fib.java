621e866d7af92fe8e1347e4394f03470560e0ce8
Hein <heinthetaung.cloud@gmail.com>
2021-06-19 13:53:09 +0800

    fibonacci recursive in java using memoization - O(n) time complexity

--- Fibonacci/Java/src/main/java/Fib.java
+++ Fibonacci/Java/src/main/java/Fib.java
@@ -1,23 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Fib {
 
     public static void getFib(long n) {
-        System.out.println(calFib(n));
+        Map<Long, Long> map = new HashMap<>();
+        System.out.println(calFib(n, map));
     }
 
-    private static long calFib(long n) {
+    private static long calFib(long n, Map<Long, Long> map) {
+        if(map.get(n) != null) {
+            return map.get(n);
+        }
         if(n <= 0) {
             return 0;
         }
         if(n <= 2) {
             return 1;
         }
-        return calFib(n-1) + calFib(n-2);
+        long n1= calFib(n-1, map);
+        map.put(n-1, n1);
+        long n2 = calFib(n-2, map);
+        map.put(n-2, n2);
+
+        return n1+ n2;
     }
 
     public static void main(String[] args) {
         for(int i =1 ; i <= 10 ; i++) {
             Fib.getFib(i);
         }
-        Fib.getFib(50);
+        Fib.getFib(100);
     }
 }
